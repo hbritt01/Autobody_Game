b5102a1d191885c8950ece3788af6ccb26913c06
Nics21 <68204549+Nics21@users.noreply.github.com>
2024-10-08 19:16:47 -0400

    Car Move Updated Files

---
+++ Assets/Scripts/Car_Move2.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car_Move2 : MonoBehaviour
+{
+    public Vector3 targetPosition = new Vector3(-2, -3, 0);
+    public Vector3 startPosition = new Vector3(3, -8, 0);
+    public float moveSpeed = 4.0f;
+    private bool shouldMove = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = startPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C)){
+            transform.position = startPosition;
+            shouldMove = true;
+        }
+
+        if (shouldMove)
+        {
+            // Move the object toward the target position
+            transform.position = Vector3.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+
+            // Stop moving once it reaches the target position
+            if (transform.position == targetPosition)
+            {
+                shouldMove = false;
+            }
+        }
+    }
+}
