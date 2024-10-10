4024c8b15bccd107699257e8792bab41afc6cc77
hbritt01 <henry.brittan@tufts.edu>
2024-10-09 23:18:55 -0400

    Created Start Scene, began on creating shop scene. Will continue later

---
+++ Assets/Scripts/GameHandler.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour {
+     public GameObject textGameObject;
+
+     public bool isEnd = true;
+
+     void Start () {
+          if (isEnd){
+               Cursor.lockState = CursorLockMode.None;
+               Cursor.visible = true;
+          }
+     }
+
+    void Update () {
+           if (Input.GetKey("escape")) {
+                 QuitGame();
+          }
+    }
+
+
+    public void PlayAgain() {
+        SceneManager.LoadScene("Garage1");
+    }
+
+    public void QuitGame() {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+    }
+}
