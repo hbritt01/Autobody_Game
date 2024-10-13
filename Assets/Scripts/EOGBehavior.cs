3bc255b6524dc5d288c37964346d9dd4cf43b969
Nics21 <68204549+Nics21@users.noreply.github.com>
2024-10-13 16:05:23 -0400

    Finalized End of Game

--- Assets/Scripts/EOGBehavior.cs
+++ Assets/Scripts/EOGBehavior.cs
@@ -5,42 +5,63 @@ using UnityEngine;
 public class EOGBehavior : MonoBehaviour
 {
     public Vector3 targetPositionW = new Vector3(0, 0, 0);
-    public Vector3 startPositionW = new Vector3(-4.5f, 0, 0);
-    public Vector3 targetScaleW = new Vector3(2f, 2f, 0);
+    public Vector3 startPositionW = new Vector3(-5f, 0, 0);
+    public Vector3 targetScaleW = new Vector3(2.2f, 2.2f, 0);
     public Vector3 startScaleW = new Vector3(1f, 2f, 0);
 
     public Vector3 targetPositionL = new Vector3(0, 0, 0);
-    public Vector3 startPositionL = new Vector3(4.5f, 0, 0);
-    public Vector3 targetScaleL = new Vector3(2.4f, 2.3f, 0);
+    public Vector3 startPositionL = new Vector3(4.9f, 0, 0);
+    public Vector3 targetScaleL = new Vector3(2.6f, 2.5f, 0);
     public Vector3 startScaleL = new Vector3(1.2f, 2.3f, 0);
 
-    public Sprite[] sprites;
-    private SpriteRenderer spriteRenderer;
-    private bool wins = true;
+    public Sprite[] sprites; // Array to hold win and lose sprites
+    public bool wins = true;
+    private bool isCoroutineRunning = false;
+
+    private GameObject winObject;
+    private GameObject loseObject;
 
     // Start is called before the first frame update
     void Start()
     {
-        if(wins){
-            transform.position = startPositionW;
-            transform.localScale = startScaleW;
-        } else {
-            transform.position = startPositionL;    
-            transform.localScale = startScaleL;
-        }
-        
+        // Create separate GameObjects for win and lose sprites
+        winObject = new GameObject("Win");
+        loseObject = new GameObject("Lose");
+
+        // Add SpriteRenderer components and set initial sprites
+        SpriteRenderer winRenderer = winObject.AddComponent<SpriteRenderer>();
+        SpriteRenderer loseRenderer = loseObject.AddComponent<SpriteRenderer>();
+
+        winRenderer.sprite = sprites[0]; // Win sprite
+        loseRenderer.sprite = sprites[1]; // Lose sprite
+
+        // Set initial positions and scales
+        winObject.transform.position = startPositionW;
+        winObject.transform.localScale = startScaleW;
+
+        loseObject.transform.position = startPositionL;
+        loseObject.transform.localScale = startScaleL;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(wins){
-            transform.position = Vector3.MoveTowards(transform.position, targetPositionW, 2 * Time.deltaTime);
-            transform.localScale = Vector3.MoveTowards(transform.localScale, targetScaleW, 1 * Time.deltaTime);
-        } else {
-            transform.position = Vector3.MoveTowards(transform.position, targetPositionL, 2 * Time.deltaTime);
-            transform.localScale = Vector3.MoveTowards(transform.localScale, targetScaleL, 1 * Time.deltaTime);
+        // Move and scale only the active GameObject
+        winObject.SetActive(true);
+        loseObject.SetActive(true);
+
+        if(wins)
+        {
+            loseObject.SetActive(false);
+            winObject.transform.position = Vector3.MoveTowards(winObject.transform.position, targetPositionW, 2 * Time.deltaTime);
+            winObject.transform.localScale = Vector3.MoveTowards(winObject.transform.localScale, targetScaleW, 1 * Time.deltaTime);
         }
-        
+        else
+        {
+            winObject.SetActive(false);
+            loseObject.transform.position = Vector3.MoveTowards(loseObject.transform.position, targetPositionL, 2 * Time.deltaTime);
+            loseObject.transform.localScale = Vector3.MoveTowards(loseObject.transform.localScale, targetScaleL, 1 * Time.deltaTime);
+        }
     }
 }
+
