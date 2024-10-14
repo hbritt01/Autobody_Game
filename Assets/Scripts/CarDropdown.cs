5593ee201b8fd2e610a8ee39a39934d1630a4b91
hbritt01 <henry.brittan@tufts.edu>
2024-10-14 14:47:31 -0400

    Made progress adding dropdown to car.
    Still need to make it trigger when you walk near car, but it randomly adds issues that car is having as of now.

---
+++ Assets/Scripts/CarDropdown.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarDropdown : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    //take in list, and does whatever to list
+    public List<string> dropdownVals;
+    void Start()
+    {
+        
+    }
+    void setValues(List<string> toCopy)
+    {
+
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/CarIssue1.cs
+++ Assets/Scripts/CarIssue1.cs
@@ -1,23 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CarIssue1 : MonoBehaviour
 {
 
     private int numProblems;
-    private List<string> thatCarsProblems;
+    [SerializeField] private List<string> thatCarsProblems;
+    [SerializeField] private Dropdown cardropdown;
+
+    // public BoxCollider2D collision;
     // private string carProblems[] = {"Flat Tire" , "Exploded Engine" , "Oil Change Required" , "Paint Job Wanted", "Mirror Smashed"};
     // Start is called before the first frame update
     void Start()
     {
         SetProblems();
-        
+        setDropdown();
     }
     void SetProblems()
     {
-        numProblems = Random.Range(0,4);
-
+        numProblems = Random.Range(1,4);
         Debug.Log("We have " + numProblems + " problems");
         ArrayList carProblems = new ArrayList(); 
         carProblems.Add("Flat Tire");
@@ -28,9 +31,30 @@ public class CarIssue1 : MonoBehaviour
 
         for (int i = 0; i < numProblems; i++) {  
             // string issue = (string)carProblems[i];
+            Debug.Log("NumProblems: " + numProblems);
             thatCarsProblems.Add((string)carProblems[i]);
             carProblems.RemoveAt(i);
+            foreach (string s in thatCarsProblems) {
+                Debug.Log("Problem is " + s);
+            }
         }
     }
-    // Update is called once per frame
+
+    void setDropdown()
+    {
+        // Dropdown cardropdown = GetComponent<Dropdown>();
+        foreach (string s in thatCarsProblems) {
+            cardropdown.ClearOptions();
+            cardropdown.AddOptions(thatCarsProblems);
+        }
+
+    }
+    void ShowMenu()
+    {
+        
+    }
+    void OnTriggerEnter2D(Collider2D other)
+        { 
+            Debug.Log("ran into car");
+        }
 }
