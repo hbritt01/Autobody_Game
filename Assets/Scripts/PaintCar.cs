cc312bc4856c638008c075fedb69ae8cba1c177e
alexanderjaelee <154767101+alexanderjaelee@users.noreply.github.com>
2024-10-13 22:58:22 -0400

    bug fixes Paint MG

--- Assets/Scripts/PaintCar.cs
+++ Assets/Scripts/PaintCar.cs
@@ -1,50 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System.Windows;
-using System.Windows.Media;
 
 public class NewBehaviourScript : MonoBehaviour
 {
     public Texture2D texture;
-    public PolygonCollider[] polygonColliders;
+    public PolygonCollider2D[] polygonColliders;
     public int brushSize = 5;
     public string[] paintColor = {"#FFFF0000", "#FF0000FF", "#FF008000", "#FF6495ED", "#FFDDA0DD", "#FFFFD700", "#FF2F4F4F", "#FF800000",
     "#FFA0522D", "#FF4B0082"};
-    public gameObject[] selectedColor;
-    public int selectedIndex = MIN_INT;
-    public Vector2Int TextureSize;
+    public GameObject[] selectedColor;
+    public int selectedIndex = -1;
+    public Vector2Int textureSize;
     // Start is called before the first frame update
     void Start()
     {
-        TextureSize = new Vector2Int(texture.width, texture.height);
+        textureSize = new Vector2Int(texture.width, texture.height);
         GetComponent<Renderer>().material.mainTexture = texture;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseDown(0))
+        if (Input.GetMouseButtonDown(0))
         {
             PaintPixels();
         }
-        ColorSelector();
     }
     void PaintPixels() {
         // create a ray through mouse position
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition)
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         
-        if (Physics.Raycast(ray, out hit))
+        if (Physics.Raycast(ray, out RaycastHit hit))
         {
-            if (hit.collider == polygonColliders[0] || hit.collider == polygonColliders[1] || hit.collider == polygonColliders[2])
+            if (hit.collider == polygonColliders[0] || hit.collider == polygonColliders[1] || hit.collider == polygonColliders[2]) {
                 Vector2 uv = hit.textureCoord;
-            else if (hit.collider == )
-            int x = Mathf.FloorToInt(uv.x * textureSize.x);
-            int y = Mathf.FloorToInt(uv.y * textureSize.y);
-            if (selectedIndex >= 0 && ColorUtility.TryToParse(paintColor[selectedIndex], out Color color))
-            {
-                PaintCircle(x, y, color);
-                texture.Apply();
+                int x = Mathf.FloorToInt(uv.x * textureSize.x);
+                int y = Mathf.FloorToInt(uv.y * textureSize.y);
+                if (selectedIndex >= 0 && ColorUtility.TryParseHtmlString(paintColor[selectedIndex], out Color color))
+                {
+                    PaintCircle(x, y, color);
+                    texture.Apply();
+                }
             }
         }
     }
@@ -56,8 +53,8 @@ public class NewBehaviourScript : MonoBehaviour
                 {
                     int pixelX = centerX + x;
                     int pixelY = centerY + y;
-                    if (pixelX >= 0 && pixelY < textureSize.x && pixelY >= pixelY < textureSize.y) 
-                        textureSize.SetPixel(pixelX, pixelY, color);
+                    if (pixelX >= 0 && pixelY < textureSize.x && pixelY >= 0 &&pixelY < textureSize.y)
+                        texture.SetPixel(pixelX, pixelY, color);
                 }
             }
         }
